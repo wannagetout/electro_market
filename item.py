596d1bb07ca976c5bf1a4e51ca942f0e81fcacea
wannagetout <inv.moiseev@gmail.com>
2023-02-25 01:42:22 +0300

    Static methods, property and tests

--- item.py
+++ item.py
@@ -1,9 +1,12 @@
+import csv
+
+
 class Item:
 	discount = 1
 	items = []
 
 	def __init__(self, item_name, price, amount):
-		self.item_name = item_name
+		self.__item_name = item_name
 		self.price = price
 		self.amount = amount
 		Item.items.append(self)
@@ -14,3 +17,24 @@ class Item:
 	def calculate_discount(self):
 		self.price = int(self.price * self.discount)
 		return self.price
+
+	@staticmethod
+	def instantiate_from_csv():
+		with open('items.csv', 'r', encoding='windows-1251') as file:
+			file = csv.DictReader(file, dialect='excel')
+			for row in file:
+				Item(item_name=row['name'], price=row['price'], amount=row['quantity'])
+
+	@staticmethod
+	def is_integer(value):
+		return value.is_integer()
+
+	@property
+	def item_name(self):
+		if len(self.__item_name) <= 10:
+			return self.__item_name
+		raise ValueError('Длина наименования товара превышает 10 символов.')
+
+	@item_name.setter
+	def item_name(self, value):
+		self.__item_name = value
